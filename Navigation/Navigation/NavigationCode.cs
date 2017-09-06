1da5f49721f4135cdcf985079ddcf98f7ae3f142
Fred <domaine75@gmail.com>
2017-09-06 09:46:51 +0100

    Initial commit

---
+++ Navigation/Navigation/NavigationCode.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace Navigation
+{
+    public class NavigationCode : ContentPage
+    {
+        public NavigationCode()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin Forms!" }
+                }
+            };
+        }
+    }
+}
---
+++ Navigation/Navigation/StartPage.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace Navigation
+{
+    internal class StartPage:TabbedPage
+    {
+        private StartPage()
+        {
+            Children.Add(new XFNavigation());
+            Children.Add(new NavigationCode());
+
+        }
+    }
+}
